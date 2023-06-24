c44521294db9315375bc136e39b7327aa700587d
developerom52 <114483096+developerom52@users.noreply.github.com>
2023-06-24 17:39:17 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,73 @@
+# DataImport.js
+
+Simple JavaScript browser-based CSV importer. 
+
+Inspired by Patrick McKenzie's [Design and Implementation of CSV/Excel Upload for SaaS](https://www.kalzumeus.com/2015/01/28/design-and-implementation-of-csvexcel-upload-for-saas/). DataImport is built on top of [Handsontable](https://handsontable.com/) and lets you do CSV input data pre-validation in the browser.
+
+[Demo on JSFiddle](https://jsfiddle.net/gh/get/library/pure/burnash/dataimport/tree/master/demo/) (Click **Run** or Cmd/Ctrl + Enter to launch it)
+
+## Example 
+
+```javascript
+var is = DataImport.is;
+
+var containerElement = document.getElementById("handsontable-element");
+
+var dataimport = new DataImport(containerElement, {
+  fields: [{
+    id: "fullName",
+    name: "Full Name",
+    required: true
+  }, {
+    id: "birthday",
+    name: "Birthday",
+    required: false,
+  }, {
+    id: "contactType",
+    name: "Contact Type",
+    required: true,
+    choices: ["work", "home"],
+    validate: [
+      is.anyOf(["work", "home"], "Wrong value")
+    ]
+  }, {
+    id: "email",
+    name: "Email",
+    required: true,
+    validate: [
+      is.unique(),
+      is.matchingRegex(["[^@]+@[^\.]+\..+"], "Incorrect Email Address")
+    ]
+  }],
+  // ...
+});
+
+dataimport.validate({
+  complete: function (result) {
+    console.log(result);
+  },
+  fail: function (errors) {
+    console.log(errors);
+  }
+});
+```
+
+## How it works
+
+Here's what happens in the demo.
+
+1. When the user adds a CSV file the fields in the file are matched to the `fields` described in `DataImport`. This example uses [Papa Parse](https://www.papaparse.com/) to load the data from CSV and [Fuse.js](https://fusejs.io/) to do fuzzy matching of fields.
+
+![alt text](demo/example-loaded-csv.png)
+
+2. If needed the user can fix automatic matching by selecting a proper field from the dropdown.
+
+![alt text](demo/example-dropdown.png)
+
+3. When the user clicks Validate, DataImport checks the data and displays the errors.
+
+![alt text](demo/example-validation-errors.png)
+
+4. When there are no errors `DataImport.validate` calls `complete` callback with resulting data (array of arrays). This is your parced CSV data.
+
+Check [Demo on JSFiddle](https://jsfiddle.net/gh/get/library/pure/burnash/dataimport/tree/master/demo/) (Click **Run** or Cmd/Ctrl + Enter to launch it)
---
+++ gulpfile.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var gulp = require('gulp'),
+  hint = require('gulp-jshint'),
+  stylish = require('jshint-stylish'),
+  rename = require('gulp-rename'),
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload,
+  concat = require('gulp-concat'),
+  debug = require('gulp-debug'),
+  sourcemaps = require('gulp-sourcemaps');
+
+var DEV_ROOT = 'build/dev',
+  DIST_ROOT = 'build/dist',
+  DIST_NAME = 'dataimport';
+
+var src = {
+  vendorCSS: 'vendor_modules/**/*.css',
+  vendorJS: [
+    'vendor_modules/jquery.min.js',
+    'vendor_modules/handsontable.full.js',
+    'vendor_modules/papaparse.js',
+    'vendor_modules/fuse.js',
+  ],
+  js: [
+    'src/scripts/sheet.js',
+    'src/scripts/dataimport.js',
+    'src/scripts/validators.js',
+  ],
+  devJS: [
+    'src/scripts/dropfile.js',
+    'src/scripts/main.js'
+  ],
+  css: ['src/css/dataimport.css'],
+  devCSS: ['src/css/main.css'],
+  html: 'src/*.html',
+};
+
+gulp.task('hint', function () {
+  return gulp.src(src.js).pipe(hint('.jshintrc'))
+    .pipe(hint.reporter(stylish))
+    .pipe(hint.reporter('fail'));
+});
+
+gulp.task('vendor:dev', function () {
+  return gulp.src(src.vendorJS)
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(DEV_ROOT));
+});
+
+gulp.task('scripts:dev', ['hint', 'vendor:dev'], function () {
+  return gulp.src(src.js.concat(src.devJS))
+    .pipe(debug())
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(DEV_ROOT))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('scripts:dist', ['hint'], function () {
+  return gulp.src(src.js)
+    .pipe(concat(DIST_NAME + '.js'))
+    .pipe(gulp.dest(DIST_ROOT));
+});
+
+gulp.task('styles:dev', function () {
+  return gulp.src(src.vendorCSS.concat(src.css))
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest(DEV_ROOT))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('styles:dist', function () {
+  return gulp.src(src.css)
+    .pipe(concat(DIST_NAME + '.css'))
+    .pipe(gulp.dest(DIST_ROOT));
+});
+
+gulp.task('html:dev', function () {
+  return gulp.src('src/dev.html')
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest(DEV_ROOT))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('serve', function () {
+  browserSync({
+    server: DEV_ROOT
+  });
+
+  gulp.watch(src.js.concat(src.devJS), ['scripts:dev']);
+  gulp.watch(src.css, ['styles:dev']);
+  gulp.watch(src.html, ['html:dev']);
+});
+
+gulp.task('build', [
+  'scripts:dist', 'styles:dist'
+]);
